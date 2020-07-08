0ab5456eef0a588aad1f9bbfd36578d596474b9f
TylerNW3994 <TylerNW3994@gmail.com>
2020-07-08 19:50:58 -0400

    Allows player to draw a card

--- Assets/Scripts/PlayCards.cs
+++ Assets/Scripts/PlayCards.cs
@@ -1,16 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayCards : MonoBehaviour
 {
+    private int pullCardIndex;
+
     public static PlayCards instance;
 
     void Start(){
         instance = this;
     }
-    
-    public void DrawCards(){
 
+    public Card DrawCard(){
+        Card playedCard = GameManager.instance.deck[0];
+        GameManager.instance.deck.RemoveAt(0);
+        return playedCard;
     }
 }
