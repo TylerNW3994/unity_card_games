8bb6a717737fd63c06bc34f21ea7c513bc114a58
TylerNW3994 <TylerNW3994@gmail.com>
2020-07-08 18:32:51 -0400

    Adding files

---
+++ Assets/Scripts/PlayCards.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayCards : MonoBehaviour
+{
+    public static PlayCards instance;
+
+    void Start(){
+        instance = this;
+    }
+    
+    public void DrawCards(){
+
+    }
+}
